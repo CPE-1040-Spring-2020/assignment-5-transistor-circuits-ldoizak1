b2dae085f8a3147903552d4839a481022a01e9e5
ldoizak1 <47997282+ldoizak1@users.noreply.github.com>
2020-03-11 16:40:25 -0600

    Create digital-in.js

---
+++ digital-in.js
@@ -0,0 +1,8 @@
+basic.forever(function () {
+    if (pins.digitalReadPin(DigitalPin.P12)) {
+        pins.analogWritePin(AnalogPin.P0, 1023)
+    }
+    else
+        pins.analogWritePin(AnalogPin.P0, 0)
+    basic.pause(1000)
+})
